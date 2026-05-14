0814c8c0d3f591210531ac871330caa3a6dace55
Sherinthanseer <sherinthanseer@gmail.com>
2026-05-14 12:14:49 +0530

    connected deployed backend

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5002/api",
+  baseURL: "https://thinkpad-backend-m933.onrender.com/api",
 });
 
 export default api;
